cf783a80c813c1e7f26e2e4e66437ab154015751
samescolas <samescolas@gmail.com>
2017-06-28 17:23:23 -0700

    Fixed validation issues

--- srcs/utils/validation.c
+++ srcs/utils/validation.c
@@ -6,9 +6,11 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/28 16:16:53 by sescolas          #+#    #+#             */
-/*   Updated: 2017/06/28 16:25:24 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/06/28 17:21:01 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../libft/libft.h"
 
 int		valid_brackets(char *str)
 {
@@ -23,19 +25,26 @@ int		valid_brackets(char *str)
 			--count;
 		++str;
 	}
-	return (count == 0);
+	if (count == 0)
+		return (1);
+	return (0);
 }
 
 int		valid_quotes(char *str)
 {
-	int		count;
+	int		count[2];
 
-	count = 0;
+	count[0] = 0;
+	count[1] = 0;
 	while (*str != '\0')
 	{
-		if (*str == '"' || *str == '\'')
-			++count;
+		if (*str == '"')
+			++count[0];
+		else if (*str == '\'')
+			++count[1];
 		++str;
 	}
-	return (count == 0);
+	if (count[0] % 2 == 0 && count[1] % 2 == 0)
+		return (1);
+	return (0);
 }
