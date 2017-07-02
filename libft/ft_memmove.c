d4a752c490ae4520cb8e78bed9092693ccc7dbdf
samescolas <samescolas@gmail.com>
2017-07-02 13:16:51 -0700

    Finally made my ft_memmove less lazy

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,22 +6,32 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 17:05:02 by sescolas          #+#    #+#             */
-/*   Updated: 2017/01/20 10:32:26 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/07/02 13:16:21 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** If shifting left copy from left to right. otherwise copy right to left.
+*/
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	void	*tmp;
+	int		i;
 
-	tmp = malloc(len);
-	if (tmp)
+	if (dst < src)
 	{
-		ft_memcpy(tmp, src, len);
-		ft_memcpy(dst, tmp, len);
+		i = -1;
+		while (++i < (int)len)
+			ft_memset((unsigned char *)(dst + i), (unsigned char)*((unsigned char *)src + i), 1);
 	}
-	free(tmp);
+	else if (dst > src)
+	{
+		i = (int)len;
+		while (i--)
+			ft_memset(
+				(unsigned char *)((unsigned char *)dst + i),
+				(unsigned char)*((unsigned char *)src + i), 1);
+	}
 	return (dst);
 }
