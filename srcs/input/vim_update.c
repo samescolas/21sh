7a92c69fc2b23a7a1baca28d5afcc295292185ba
samescolas <samescolas@gmail.com>
2017-09-01 18:36:00 -0700

    Adding code for vim arrowkeys.

--- srcs/input/vim_update.c
+++ srcs/input/vim_update.c
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/25 12:28:47 by sescolas          #+#    #+#             */
-/*   Updated: 2017/06/26 12:21:17 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/09/01 18:28:37 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 #include "sftsh_types.h"
 #include "ft_termcap.h"
 #include "../../libft/libft.h"
+
 /*
-int		update_vimarrow(int key, t_sess *sess)
+int		update_vimarrow(int key, t_sh *shell)
 {
 	if (key == 'h' || key == KEY_LEFT)
 		return (update_arrowkey(KEY_LEFT, sess));
@@ -27,35 +28,49 @@ int		update_vimarrow(int key, t_sess *sess)
 		return (update_arrowkey(KEY_UP, sess));
 	return (1);
 }
+*/
 
-int		update_vimword(int key, t_sess *sess)
+int		update_vimarrow(int key, t_sh *shell)
+{
+	if (key == 'h')
+		key = KEY_LEFT;
+	else if (key == 'l')
+		key = KEY_RIGHT;
+	else if (key == 'j')
+		key = KEY_DOWN;
+	else if (key == 'k')
+		key = KEY_UP;
+	return (update_arrowkey(key, shell));
+}
+
+int		update_vimword(int key, t_sh *shell)
 {
 	if (ft_toupper(key) == 'W')
 	{
-		if (sess->cursor->x == sess->input_len)
+		if (shell->ix->x == shell->input[shell->ix->y]->len)
 			return (1);
 		if (key == 'W')
-			sess->cursor->x += ft_strfind(&sess->input_text[sess->cursor->x + 1], ' ');
+			shell->ix->x += ft_strfind(&shell->input[shell->ix->y][shell->ix->x + 1], ' ');
 		else
 		{
-			while (ft_isalpha(sess->input_text[sess->cursor->x]))
-				++(sess->cursor->x);
+			while (ft_isalpha(shell->input[shell->ix->y][shell->ix->x]))
+				++(shell->ix->x);
 		}
 	}
 	else if (ft_toupper(key) == 'B')
 	{
-		if (sess->cursor->x == 0)
+		if (shell->ix->x == 0)
 			return (1);
 		if (key == 'B')
 		{
-			while (sess->cursor->x > 0)
-				if (ft_isalpha(sess->input_text[--(sess->cursor->x)]))
+			while (shell->ix->x > 0)
+				if (ft_isalpha(shell->input[shell->ix->y][--(shell->ix->x)]))
 					break ;
 		}
 		else
 		{
-			while (sess->cursor->x > 0)
-				if (sess->input_text[--(sess->cursor->x)] == ' ')
+			while (shell->ix->x > 0)
+				if (shell->input[shell->ix->y][--(shell->ix->x)] == ' ')
 					break ;
 		}
 	}
@@ -63,4 +78,3 @@ int		update_vimword(int key, t_sess *sess)
 		return (1);
 	return (0);
 }
-*/
