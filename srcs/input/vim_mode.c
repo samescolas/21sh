ff0282d3853eb38d28edd2f684ded95d9e9c5323
samescolas <samescolas@gmail.com>
2017-09-01 18:35:34 -0700

    Replacing old struct code with new struct. Code needs to change..

--- srcs/input/vim_mode.c
+++ srcs/input/vim_mode.c
@@ -6,33 +6,33 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/25 12:13:21 by sescolas          #+#    #+#             */
-/*   Updated: 2017/06/26 12:21:29 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/09/01 18:19:31 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sftsh_read_line.h"
 #include "sftsh_types.h"
 #include "../../libft/libft.h"
-/*
-static int	process_vimkey(int key, t_sess *sess)
+
+static int	process_vimkey(int key, t_sh *shell)
 {
 	if (is_vimarrow(key))
-		return (update_vimarrow(key, sess));
+		return (update_vimarrow(key, shell));
 	else if (ft_toupper(key) == 'W' || ft_toupper(key) == 'B')
-		return (update_vimword(key, sess));
+		return (update_vimword(key, shell));
 	return (0);
 }
 
-static int	render_vimkey(int key, t_sess *sess)
+static int	render_vimkey(int key, t_sh *shell)
 {
 	if (is_vimarrow(key))
 		return (render_vimarrow(key));
 	else if (ft_toupper(key) == 'W' || ft_toupper(key) == 'B')
-		return (render_vimword(sess));
-	return (sess != (void *)0);
+		return (render_vimword(shell));
+	return (shell != (void *)0);
 }
 
-int		enter_vim_mode(t_sess *sess)
+int		enter_vim_mode(t_sh *shell)
 {
 	int		key;
 
@@ -40,12 +40,11 @@ int		enter_vim_mode(t_sess *sess)
 	{
 		if (key == '\0')
 			continue ;
-		if (process_vimkey(key, sess) == 0)
+		if (process_vimkey(key, shell) == 0)
 		{
-			if (render_vimkey(key, sess) != 0)
+			if (render_vimkey(key, shell) != 0)
 				return (1);
 		}
 	}
 	return (key == KEY_ENTER);
 }
-*/
